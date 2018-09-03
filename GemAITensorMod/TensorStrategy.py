58f229bba6d2fb604765f3febc96ff9f7cee0234
arbk <arbelkf@gmail.com>
2018-09-03 11:19:01 +0200

    git remote -v

--- GemAITensorMod/TensorStrategy.py
+++ GemAITensorMod/TensorStrategy.py
@@ -2,134 +2,139 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import definitions
+import os
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 
-# Import data
-data = pd.read_csv('data_stocks.csv')
 
-# Drop date variable
-data = data.drop(['DATE'], 1)
+class TensorStrategy():
+    def DoStrategy(self, ticker):
+        filename = os.path.join(definitions.IMPORTLocationFiles , '{}.csv'.format(ticker))
+        data = pd.read_csv(filename)
 
-# Dimensions of dataset
-n = data.shape[0]
-p = data.shape[1]
+        # Drop date variable
+        data = data.drop(['DATE'], 1)
 
-# Make data a np.array
-data = data.values
+        # Dimensions of dataset
+        n = data.shape[0]
+        p = data.shape[1]
 
-# Training and test data
-train_start = 0
-train_end = int(np.floor(0.8*n))
-test_start = train_end + 1
-test_end = n
-data_train = data[np.arange(train_start, train_end), :]
-data_test = data[np.arange(test_start, test_end), :]
+        # Make data a np.array
+        data = data.values
 
-# Scale data
-scaler = MinMaxScaler(feature_range=(-1, 1))
-scaler.fit(data_train)
-data_train = scaler.transform(data_train)
-data_test = scaler.transform(data_test)
+        # Training and test data
+        train_start = 0
+        train_end = int(np.floor(0.8*n))
+        test_start = train_end + 1
+        test_end = n
+        data_train = data[np.arange(train_start, train_end), :]
+        data_test = data[np.arange(test_start, test_end), :]
 
-# Build X and y
-X_train = data_train[:, 1:]
-y_train = data_train[:, 0]
-X_test = data_test[:, 1:]
-y_test = data_test[:, 0]
+        # Scale data
+        scaler = MinMaxScaler(feature_range=(-1, 1))
+        scaler.fit(data_train)
+        data_train = scaler.transform(data_train)
+        data_test = scaler.transform(data_test)
 
-# Number of stocks in training data
-n_stocks = X_train.shape[1]
+        # Build X and y
+        X_train = data_train[:, 1:]
+        y_train = data_train[:, 0]
+        X_test = data_test[:, 1:]
+        y_test = data_test[:, 0]
 
-# Neurons
-n_neurons_1 = 1024
-n_neurons_2 = 512
-n_neurons_3 = 256
-n_neurons_4 = 128
+        # Number of stocks in training data
+        n_stocks = X_train.shape[1]
 
-# Session
-net = tf.InteractiveSession()
+        # Neurons
+        n_neurons_1 = 1024
+        n_neurons_2 = 512
+        n_neurons_3 = 256
+        n_neurons_4 = 128
 
-# Placeholder
-X = tf.placeholder(dtype=tf.float32, shape=[None, n_stocks])
-Y = tf.placeholder(dtype=tf.float32, shape=[None])
+        # Session
+        net = tf.InteractiveSession()
 
-# Initializers
-sigma = 1
-weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distribution="uniform", scale=sigma)
-bias_initializer = tf.zeros_initializer()
+        # Placeholder
+        X = tf.placeholder(dtype=tf.float32, shape=[None, n_stocks])
+        Y = tf.placeholder(dtype=tf.float32, shape=[None])
 
-# Hidden weights
-W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
-bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
-W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
-bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
-W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
-bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
-W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
-bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
+        # Initializers
+        sigma = 1
+        weight_initializer = tf.variance_scaling_initializer(mode="fan_avg", distribution="uniform", scale=sigma)
+        bias_initializer = tf.zeros_initializer()
 
-# Output weights
-W_out = tf.Variable(weight_initializer([n_neurons_4, 1]))
-bias_out = tf.Variable(bias_initializer([1]))
+        # Hidden weights
+        W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
+        bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
+        W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
+        bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
+        W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
+        bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
+        W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
+        bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
 
-# Hidden layer
-hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
-hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
-hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
-hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
+        # Output weights
+        W_out = tf.Variable(weight_initializer([n_neurons_4, 1]))
+        bias_out = tf.Variable(bias_initializer([1]))
 
-# Output layer (transpose!)
-out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
+        # Hidden layer
+        hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
+        hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
+        hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
+        hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
 
-# Cost function
-mse = tf.reduce_mean(tf.squared_difference(out, Y))
+        # Output layer (transpose!)
+        out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
 
-# Optimizer
-opt = tf.train.AdamOptimizer().minimize(mse)
+        # Cost function
+        mse = tf.reduce_mean(tf.squared_difference(out, Y))
 
-# Init
-net.run(tf.global_variables_initializer())
+        # Optimizer
+        opt = tf.train.AdamOptimizer().minimize(mse)
 
-# Setup plot
-plt.ion()
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-line1, = ax1.plot(y_test)
-line2, = ax1.plot(y_test * 0.5)
-plt.show()
+        # Init
+        net.run(tf.global_variables_initializer())
 
-# Fit neural net
-batch_size = 256
-mse_train = []
-mse_test = []
+        # Setup plot
+        plt.ion()
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        line1, = ax1.plot(y_test)
+        line2, = ax1.plot(y_test * 0.5)
+        plt.show()
 
-# Run
-epochs = 10
-for e in range(epochs):
+        # Fit neural net
+        batch_size = 256
+        mse_train = []
+        mse_test = []
 
-    # Shuffle training data
-    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
-    X_train = X_train[shuffle_indices]
-    y_train = y_train[shuffle_indices]
+        # Run
+        epochs = 10
+        for e in range(epochs):
 
-    # Minibatch training
-    for i in range(0, len(y_train) // batch_size):
-        start = i * batch_size
-        batch_x = X_train[start:start + batch_size]
-        batch_y = y_train[start:start + batch_size]
-        # Run optimizer with batch
-        net.run(opt, feed_dict={X: batch_x, Y: batch_y})
+            # Shuffle training data
+            shuffle_indices = np.random.permutation(np.arange(len(y_train)))
+            X_train = X_train[shuffle_indices]
+            y_train = y_train[shuffle_indices]
 
-        # Show progress
-        if np.mod(i, 50) == 0:
-            # MSE train and test
-            mse_train.append(net.run(mse, feed_dict={X: X_train, Y: y_train}))
-            mse_test.append(net.run(mse, feed_dict={X: X_test, Y: y_test}))
-            print('MSE Train: ', mse_train[-1])
-            print('MSE Test: ', mse_test[-1])
-            # Prediction
-            pred = net.run(out, feed_dict={X: X_test})
-            line2.set_ydata(pred)
-            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
-            plt.pause(0.01)
+            # Minibatch training
+            for i in range(0, len(y_train) // batch_size):
+                start = i * batch_size
+                batch_x = X_train[start:start + batch_size]
+                batch_y = y_train[start:start + batch_size]
+                # Run optimizer with batch
+                net.run(opt, feed_dict={X: batch_x, Y: batch_y})
+
+                # Show progress
+                if np.mod(i, 50) == 0:
+                    # MSE train and test
+                    mse_train.append(net.run(mse, feed_dict={X: X_train, Y: y_train}))
+                    mse_test.append(net.run(mse, feed_dict={X: X_test, Y: y_test}))
+                    print('MSE Train: ', mse_train[-1])
+                    print('MSE Test: ', mse_test[-1])
+                    # Prediction
+                    pred = net.run(out, feed_dict={X: X_test})
+                    line2.set_ydata(pred)
+                    plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
+                    plt.pause(0.01)
--- Main.py
+++ Main.py
@@ -6,14 +6,25 @@
 # StrategyModule - diffrent strategies to decide about the labels
 # GemAIModule - runs diffrent kinds of prediction procedures
 from ImportMod.Import import ImportUtil
+from GemAITensorMod.TensorStrategy import TensorStrategy
 import sys
+import logging
 
-print("Write \"import\" - to imp[ort all tickers")
+print("Write \"import\" - to import all tickers")
 str = sys.stdin.readline()
 while(str != "END\n"):
-    strparam = str.split()
-    if (strparam[0] == "import"):
-        cls = ImportUtil()
-        cls.GetAllSymbols()
-        print("END")
+    try:
+        strparam = str.split()
+        if (strparam[0] == "import"):
+            cls = ImportUtil()
+            cls.GetAllSymbols()
+            print("END")
+        if (strparam[0] == "chk"):
+            ticker = strparam[1]
+            cls = TensorStrategy()
+            cls.DoStrategy(ticker)
+    except Exception as inst:
+        msg = "args:", inst.args[0]
+        print(msg)
+        logging.error(msg)
     str = sys.stdin.readline()
